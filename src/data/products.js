de6e3e967edc1403f9cd68844297486a8a9f3a99
kathawate <srkathawate@gmail.com>
2026-04-02 17:16:27 +0530

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Punjabi Patiala Kurti Set",
+    price: "₹2,499",
+    img: "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?w=400&h=500&fit=crop",
+  },
+  {
+    id: 2,
+    name: "Phulkari Kurti",
+    price: "₹1,999",
+    img: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=400&h=500&fit=crop",
+  },
+  {
+    id: 3,
+    name: "Embroidered Punjabi Suit",
+    price: "₹3,299",
+    img: "https://images.unsplash.com/photo-1621252179027-94459d278660?w=400&h=500&fit=crop",
+  },
+  {
+    id: 4,
+    name: "Cotton Punjabi Kurti",
+    price: "₹1,499",
+    img: "https://images.unsplash.com/photo-1593032465175-481ac7f401a0?w=400&h=500&fit=crop",
+  },
+];
